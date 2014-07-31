f1cda7c686eee424e6b6edb73a95bc8d410b0f71
Doug Swisher <big.swish@gmail.com>
2014-07-30 21:15:46 -0500

    Set up unit test for wildcard array match (TBD)

---
+++ src/Difftaculous.Test/JsonPathTests.cs
@@ -0,0 +1,28 @@
+﻿
+using Difftaculous.Paths;
+using NUnit.Framework;
+
+
+namespace Difftaculous.Test
+{
+    [TestFixture]
+    public class JsonPathTests
+    {
+        // TODO - is matching symmetric?  Does a.Matches(b) => b.Matches(a)?
+
+        [Ignore("Get this working!")]
+        [TestCase("$.countries[1].points")]
+        [TestCase("$.countries[55].points")]
+        // [TestCase("$.countries[sort='x'].points")]
+        public void WildcardArrayMatch(string s)
+        {
+            var template = DiffPath.FromJsonPath("$.countries[*].points");
+            var path = DiffPath.FromJsonPath(s);
+
+            // TODO - how to write this with shouldly, in such a way that we see both paths on failure?
+            // path.Matches(template).ShouldBe(true);
+
+            Assert.That(template.Matches(path), string.Format("{0} matches {1}", template.AsJsonPath, path.AsJsonPath));
+        }
+    }
+}
