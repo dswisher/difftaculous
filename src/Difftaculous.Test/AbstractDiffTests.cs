201c1d94a8d208587ba0ab04de0fc61922b50a53
Doug Swisher <big.swish@gmail.com>
2014-07-31 20:49:56 -0500

    Move over two more tests

--- src/Difftaculous.Test/AbstractDiffTests.cs
+++ src/Difftaculous.Test/AbstractDiffTests.cs
@@ -68,10 +68,79 @@ namespace Difftaculous.Test
         [Test]
         public void SimpleArrayComparedWithItselfHasNoDifferences()
         {
-            // const string a = "[\"Red\", \"Green\", \"Blue\"]";
             string[] a = { "Red", "Green", "Blue" };
             string[] b = new string[3];
             Array.Copy(a, b, 3);
+
+            var result = DoCompare(a, b);
+
+            result.AreSame.ShouldBe(true);
+        }
+
+
+
+        [Test]
+        public void AlteredSimpleArrayResultsInOneDifference()
+        {
+            string[] a = { "Red", "Green", "Blue" };
+            string[] b = { "Red", "Black", "Blue" };
+
+            var result = DoCompare(a, b);
+
+            result.AreSame.ShouldBe(false);
+
+            // TODO - verify annotation!
+            // result.Annotations.ShouldContain(x => x.Path.Equals(DiffPath.FromJsonPath("$[1]")));
+        }
+
+
+
+
+
+        public class NestedObject
+        {
+            public SimpleObject Thing { get; set; }
+        }
+
+
+
+        [Test]
+        public void AlteredNestedPropertyResultsInOneDifference()
+        {
+            var a = new NestedObject { Thing = new SimpleObject { Name = "Fred" } };
+            var b = new NestedObject { Thing = new SimpleObject { Name = "Barney" } };
+
+            var result = DoCompare(a, b);
+
+            result.AreSame.ShouldBe(false);
+
+            // TODO - verify annotation!
+            //result.Annotations.ShouldContain(x => x.Path.Equals(DiffPath.FromJsonPath("$.thing.name")));
+        }
+
+
+
+
+        public class TwoProp
+        {
+            public string Prop1 { get; set; }
+            public string Prop2 { get; set; }
+        }
+
+
+        public class TwoPropRev
+        {
+            public string Prop2 { get; set; }
+            public string Prop1 { get; set; }
+        }
+
+
+
+        [Test]
+        public void ReorderedPropertiesAreSame()
+        {
+            var a = new TwoProp {Prop1 = "val1", Prop2 = "val2"};
+            var b = new TwoPropRev { Prop1 = "val1", Prop2 = "val2" };
 
             var result = DoCompare(a, b);
 
