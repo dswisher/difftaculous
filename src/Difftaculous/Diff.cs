62e32c5a69bbf3eecb517bb854e031a868f43943
Doug Swisher <big.swish@gmail.com>
2014-08-02 22:39:11 -0500

    Start cleaning obsolete code

--- src/Difftaculous/Diff.cs
+++ src/Difftaculous/Diff.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using Difftaculous.Adapters;
 using Difftaculous.Caveats;
 using Difftaculous.Hints;
-using Difftaculous.Paths;
 using Difftaculous.Results;
 
 
@@ -38,37 +37,6 @@ namespace Difftaculous
             DiffEngine engine = new DiffEngine(caveats, hints);
 
             return engine.Diff(a.Content, b.Content);
-        }
-
-
-
-        // TODO - remove the JsonAdapter-based overloads
-        public static IDiffResult Compare(JsonAdapter a, JsonAdapter b, IEnumerable<ICaveat> caveats, IEnumerable<IHint> hints)
-        {
-            DiffEngine engine = new DiffEngine(caveats, hints);
-
-            return engine.Diff(a.Content, b.Content, DiffPath.Root);
-        }
-
-
-
-        public static IDiffResult Compare(JsonAdapter a, JsonAdapter b, IEnumerable<ICaveat> caveats)
-        {
-            return Compare(a, b, caveats, null);
-        }
-
-
-
-        public static IDiffResult Compare(JsonAdapter a, JsonAdapter b, IEnumerable<IHint> hints)
-        {
-            return Compare(a, b, null, hints);
-        }
-
-
-
-        public static IDiffResult Compare(JsonAdapter a, JsonAdapter b)
-        {
-            return Compare(a, b, null, null);
         }
     }
 }
