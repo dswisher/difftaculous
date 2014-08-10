0140c8c3732ac1be58940cc048f0a650eef392e3
Doug Swisher <big.swish@gmail.com>
2014-08-10 09:26:50 -0500

    Build tinkering

--- build/build.ps1
+++ build/build.ps1
@@ -5,16 +5,23 @@ properties {
     $baseDir  = "$buildDir\.."
     $artifactsDir = "$baseDir\artifacts\"
     $srcDir = "$baseDir\Src"
+    $majorVersion = "0.1"
+    $majorMinorVersion = "$majorVersion.3"
+    $version = GetVersion $majorMinorVersion
 }
 
 $framework = '4.0x86'
-# FormatTaskName (("-"*25) + "[{0}]" + ("-"*25))
+FormatTaskName (("-"*25) + "[{0}]" + ("-"*25))
 
 Task Default -Depends Clean, Build
 
 Task Build -Depends Clean {
-    Write-Host "Building solution" -ForegroundColor Green
-    # Exec { msbuild "$code_dir\helloworld\helloworld.sln" /t:Build /p:Configuration=Release /v:quiet /p:OutDir=$build_artifacts_dir } 
+    Write-Host -ForegroundColor Green "Updating assembly version"
+    Write-Host
+    # Update-AssemblyInfoFiles $srcDir ($majorVersion + '.0.0') $version
+
+    Write-Host "Building solution, version $version" -ForegroundColor Green
+    # Exec { msbuild "$srcDir\difftaculous.sln" /t:Build /p:Configuration=Release /v:quiet /p:OutDir=$artifactsDir } 
 }
 
 Task Clean {
@@ -28,4 +35,36 @@ Task Clean {
 
     #Write-Host "Cleaning helloworld.sln" -ForegroundColor Green
     #Exec { msbuild "$code_dir\helloworld\helloworld.sln" /t:Clean /p:Configuration=Release /v:quiet } 
+}
+
+
+function GetVersion($majorVersion)
+{
+    $now = [DateTime]::Now
+    
+    $months = (($now.Year - 2014) * 12) + $now.Month
+    $build = "{0}{1:00}{2:00}" -f $months, $now.Day, $now.Hour
+
+    return $majorVersion + "." + $build
+}
+
+
+function Update-AssemblyInfoFiles ([string] $sourceDir, [string] $assemblyVersionNumber, [string] $fileVersionNumber)
+{
+    $assemblyVersionPattern = 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
+    $fileVersionPattern = 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
+    $assemblyVersion = 'AssemblyVersion("' + $assemblyVersionNumber + '")';
+    $fileVersion = 'AssemblyFileVersion("' + $fileVersionNumber + '")';
+
+    Get-ChildItem -Path $sourceDir -r -filter AssemblyInfo.cs | ForEach-Object {
+
+        $filename = $_.Directory.ToString() + '\' + $_.Name
+        Write-Host $filename
+        $filename + ' -> ' + $version
+
+        (Get-Content $filename) | ForEach-Object {
+            % {$_ -replace $assemblyVersionPattern, $assemblyVersion } |
+            % {$_ -replace $fileVersionPattern, $fileVersion }
+        } | Set-Content $filename
+    }
 }
