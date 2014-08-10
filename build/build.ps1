148fe79f21146427e49c3a49bbb634adea482195
Doug Swisher <big.swish@gmail.com>
2014-08-10 08:37:23 -0500

    Start tinkering with psake for builds

---
+++ build/build.ps1
@@ -0,0 +1,31 @@
+﻿# Build script for difftaculous.
+
+properties { 
+    $buildDir = Split-Path $psake.build_script_file	
+    $baseDir  = "$buildDir\.."
+    $artifactsDir = "$baseDir\artifacts\"
+    $srcDir = "$baseDir\Src"
+}
+
+$framework = '4.0x86'
+# FormatTaskName (("-"*25) + "[{0}]" + ("-"*25))
+
+Task Default -Depends Clean, Build
+
+Task Build -Depends Clean {
+    Write-Host "Building solution" -ForegroundColor Green
+    # Exec { msbuild "$code_dir\helloworld\helloworld.sln" /t:Build /p:Configuration=Release /v:quiet /p:OutDir=$build_artifacts_dir } 
+}
+
+Task Clean {
+    Write-Host "Creating empty artifacts directory" -ForegroundColor Green
+    if (Test-Path $artifactsDir) 
+    {
+        rd $artifactsDir -rec -force | out-null
+    }
+
+    mkdir $artifactsDir | out-null
+
+    #Write-Host "Cleaning helloworld.sln" -ForegroundColor Green
+    #Exec { msbuild "$code_dir\helloworld\helloworld.sln" /t:Clean /p:Configuration=Release /v:quiet } 
+}
---
+++ build/runbuild.cmd
@@ -0,0 +1,6 @@
+@echo off
+
+rem powershell -Command "& { [Console]::WindowWidth = 150; [Console]::WindowHeight = 50; Start-Transcript runbuild.txt; Import-Module ..\Tools\PSake\psake.psm1; Invoke-psake .\build.ps1 %*; Stop-Transcript; }"
+
+
+powershell -Command "& { Start-Transcript runbuild.txt; Import-Module ..\Tools\PSake\psake.psm1; Invoke-psake .\build.ps1 %*; Stop-Transcript; }"
