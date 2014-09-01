bf9dfa535440de4d8655d4c24297979334566768
Doug Swisher <big.swish@gmail.com>
2014-08-31 21:46:03 -0500

    Start tinkering with LCS algo

---
+++ src/Difftaculous.Test/ArrayDiff/DiffArraySubsequencerTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Difftaculous.ArrayDiff;
+using Difftaculous.ZModel;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Difftaculous.Test.ArrayDiff
+{
+    [TestFixture]
+    public class DiffArraySubsequencerTests
+    {
+        private IArraySubsequencer _arraySubsequencer;
+
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            _arraySubsequencer = new DiffArraySubsequencer();
+        }
+
+
+
+        [Test]
+        public void EmptyArrays()
+        {
+            var list = _arraySubsequencer.ComputeSubsequences(StringToArray(""), StringToArray(""));
+
+            list.ShouldBeEmpty();
+        }
+
+
+
+        [Test, Ignore("Finish this!")]
+        public void SameArrays()
+        {
+            var list = _arraySubsequencer.ComputeSubsequences(StringToArray("abc"), StringToArray("abc"));
+
+            list.ShouldBe(new List<ElementGroup>
+            {
+                ElementGroup.Equal(0, 2, 0, 2)
+            });
+        }
+
+
+
+        [Test, Ignore("Finish this!")]
+        public void SimpleInsertion()
+        {
+            var list = _arraySubsequencer.ComputeSubsequences(StringToArray("abc"), StringToArray("ab123c"));
+
+            list.ShouldBe(new List<ElementGroup>
+            {
+                ElementGroup.Equal(0, 1, 0, 1),
+                ElementGroup.Insert(2, 4),
+                ElementGroup.Equal(2, 2, 5, 5)
+            });
+        }
+
+
+
+#if false
+            diffs = new List<Diff> { new Diff(Operation.EQUAL, "ab"), new Diff(Operation.INSERT, "123"), new Diff(Operation.EQUAL, "c") };
+            CollectionAssert.AreEqual(diffs, dmp.diff_main("abc", "ab123c", false), "diff_main: Simple insertion.");
+
+            diffs = new List<Diff> { new Diff(Operation.EQUAL, "a"), new Diff(Operation.DELETE, "123"), new Diff(Operation.EQUAL, "bc") };
+            CollectionAssert.AreEqual(diffs, dmp.diff_main("a123bc", "abc", false), "diff_main: Simple deletion.");
+
+            diffs = new List<Diff> { new Diff(Operation.EQUAL, "a"), new Diff(Operation.INSERT, "123"), new Diff(Operation.EQUAL, "b"), new Diff(Operation.INSERT, "456"), new Diff(Operation.EQUAL, "c") };
+            CollectionAssert.AreEqual(diffs, dmp.diff_main("abc", "a123b456c", false), "diff_main: Two insertions.");
+
+            diffs = new List<Diff> { new Diff(Operation.EQUAL, "a"), new Diff(Operation.DELETE, "123"), new Diff(Operation.EQUAL, "b"), new Diff(Operation.DELETE, "456"), new Diff(Operation.EQUAL, "c") };
+            CollectionAssert.AreEqual(diffs, dmp.diff_main("a123b456c", "abc", false), "diff_main: Two deletions.");
+
+            // Perform a real diff.
+            // Switch off the timeout.
+            dmp.Diff_Timeout = 0;
+            diffs = new List<Diff> { new Diff(Operation.DELETE, "a"), new Diff(Operation.INSERT, "b") };
+            CollectionAssert.AreEqual(diffs, dmp.diff_main("a", "b", false), "diff_main: Simple case #1.");
+
+            diffs = new List<Diff> { new Diff(Operation.DELETE, "Apple"), new Diff(Operation.INSERT, "Banana"), new Diff(Operation.EQUAL, "s are a"), new Diff(Operation.INSERT, "lso"), new Diff(Operation.EQUAL, " fruit.") };
+            CollectionAssert.AreEqual(diffs, dmp.diff_main("Apples are a fruit.", "Bananas are also fruit.", false), "diff_main: Simple case #2.");
+
+            diffs = new List<Diff> { new Diff(Operation.DELETE, "a"), new Diff(Operation.INSERT, "\u0680"), new Diff(Operation.EQUAL, "x"), new Diff(Operation.DELETE, "\t"), new Diff(Operation.INSERT, new string(new char[] { (char)0 })) };
+            CollectionAssert.AreEqual(diffs, dmp.diff_main("ax\t", "\u0680x" + (char)0, false), "diff_main: Simple case #3.");
+
+            diffs = new List<Diff> { new Diff(Operation.DELETE, "1"), new Diff(Operation.EQUAL, "a"), new Diff(Operation.DELETE, "y"), new Diff(Operation.EQUAL, "b"), new Diff(Operation.DELETE, "2"), new Diff(Operation.INSERT, "xab") };
+            CollectionAssert.AreEqual(diffs, dmp.diff_main("1ayb2", "abxab", false), "diff_main: Overlap #1.");
+
+            diffs = new List<Diff> { new Diff(Operation.INSERT, "xaxcx"), new Diff(Operation.EQUAL, "abc"), new Diff(Operation.DELETE, "y") };
+            CollectionAssert.AreEqual(diffs, dmp.diff_main("abcy", "xaxcxabc", false), "diff_main: Overlap #2.");
+
+            diffs = new List<Diff> { new Diff(Operation.DELETE, "ABCD"), new Diff(Operation.EQUAL, "a"), new Diff(Operation.DELETE, "="), new Diff(Operation.INSERT, "-"), new Diff(Operation.EQUAL, "bcd"), new Diff(Operation.DELETE, "="), new Diff(Operation.INSERT, "-"), new Diff(Operation.EQUAL, "efghijklmnopqrs"), new Diff(Operation.DELETE, "EFGHIJKLMNOefg") };
+            CollectionAssert.AreEqual(diffs, dmp.diff_main("ABCDa=bcd=efghijklmnopqrsEFGHIJKLMNOefg", "a-bcd-efghijklmnopqrs", false), "diff_main: Overlap #3.");
+
+            diffs = new List<Diff> { new Diff(Operation.INSERT, " "), new Diff(Operation.EQUAL, "a"), new Diff(Operation.INSERT, "nd"), new Diff(Operation.EQUAL, " [[Pennsylvania]]"), new Diff(Operation.DELETE, " and [[New") };
+            CollectionAssert.AreEqual(diffs, dmp.diff_main("a [[Pennsylvania]] and [[New", " and [[Pennsylvania]]", false), "diff_main: Large equality.");
+#endif
+
+
+
+        private ZArray StringToArray(string s)
+        {
+            return new ZArray(s.ToCharArray().Select(x => x.ToString()));
+        }
+    }
+}
---
+++ src/Difftaculous/ArrayDiff/DiffArraySubsequencer.cs
@@ -0,0 +1,91 @@
+﻿#region License
+//The MIT License (MIT)
+
+//Copyright (c) 2014 Doug Swisher
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Difftaculous.ZModel;
+
+
+namespace Difftaculous.ArrayDiff
+{
+    internal class DiffArraySubsequencer : IArraySubsequencer
+    {
+        public List<ElementGroup> ComputeSubsequences(ZArray arrayA, ZArray arrayB)
+        {
+            List<ElementGroup> list = new List<ElementGroup>();
+
+            // If both arrays are empty, this is easy...
+            if ((arrayA.Count == 0) && (arrayB.Count == 0))
+            {
+                return list;
+            }
+
+
+
+            // Return what we've got...
+            return list;
+        }
+
+
+
+        private int[,] ComputeArray(ZArray arrayA, ZArray arrayB)
+        {
+            // This uses the algorithm from http://en.wikipedia.org/wiki/Longest_common_subsequence_problem
+#if false
+function LCSLength(X[1..m], Y[1..n])
+    C = array(0..m, 0..n)
+    for i := 0..m
+       C[i,0] = 0
+    for j := 0..n
+       C[0,j] = 0
+    for i := 1..m
+        for j := 1..n
+            if X[i] = Y[j]
+                C[i,j] := C[i-1,j-1] + 1
+            else
+                C[i,j] := max(C[i,j-1], C[i-1,j])
+    return C[m,n]
+#endif
+
+            int[,] c = new int[arrayA.Count, arrayB.Count];
+
+            for (int i = 0; i < arrayA.Count; i++)
+            {
+                c[i, 0] = 0;
+            }
+
+            for (int j = 0; j < arrayB.Count; j++)
+            {
+                c[0, j] = 0;
+            }
+
+            // TODO
+
+            return c;
+        }
+    }
+}
